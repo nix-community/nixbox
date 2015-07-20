7a6f23e9b074a36120d1f50309af9f5db7902ae5
zimbatm <zimbatm@zimbatm.com>
2015-07-20 12:39:27 +0100

    Changes a lot of things, do all the install from the CD
    This allow us to run most of the commands from within the CD instead of having to instrument the keyboard too much.

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 # Make sure we are totally up to date
+nix-channel --add http://nixos.org/channels/nixos-14.12 nixos
 nix-channel --update
 nixos-rebuild switch --upgrade
 
@@ -8,12 +9,14 @@ nixos-rebuild switch --upgrade
 # pruned.
 
 for x in $(seq 0 2) ; do
-    nix-env --delete-generations old
-    nix-collect-garbage -d
+  nix-env --delete-generations old
+  nix-collect-garbage -d
 done
 
-# Zero out the disk (for better compression)
 
-echo "zeroing the disk"
+# Remove install ssh key
+rm -rf /root/.ssh /root/.packer_http
+
+# Zero out the disk (for better compression)
 dd if=/dev/zero of=/EMPTY bs=1M
 rm -rf /EMPTY
