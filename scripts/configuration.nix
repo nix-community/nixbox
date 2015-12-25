b491b839d010531a7895b8a252fbbeb6e109675c
princeofdarkness76  script type="text/javascript" src="https://cdn.ywxi.net/js/1.js" async/script <djsteviejames@hotmail.com>
2015-12-25 05:42:38 -0800

    Merge remote-tracking branch 'origin/nixos-14.12' into pod/nixbox-update
    # Conflicts:
    #	scripts/configuration.nix

--- scripts/configuration.nix
+++ scripts/configuration.nix
@@ -17,7 +17,10 @@
   boot.initrd.checkJournalingFS = false;
 
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
+=======
+>>>>>>> origin/nixos-14.12
   # Vagrant cannot yet handle udev's new predictable interface names.
   # Use the old ethX naming system for the moment.
   # http://www.freedesktop.org/wiki/Software/systemd/PredictableNetworkInterfaceNames/
