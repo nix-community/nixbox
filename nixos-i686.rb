3390f45b93d593dacf94cb92a39a4b27c0aa53da
zimbatm <zimbatm@zimbatm.com>
2016-02-26 17:03:18 +0000

    Update ISO urls

--- nixos-i686.rb
+++ nixos-i686.rb
@@ -3,7 +3,7 @@ require './gen_template.rb'
 
 gen_template(
   arch: "i686",
-  iso_url: "https://nixos.org/releases/nixos/15.09/nixos-15.09.597.71e29b4/nixos-minimal-15.09.597.71e29b4-i686-linux.iso",
-  iso_sha256: "659736e766149a49c6b8c3f24c47f168527e88f1aac0754bc5f4ae73ef7f0b0b",
+  iso_url: "https://nixos.org/releases/nixos/15.09/nixos-15.09.1076.9220f03/nixos-minimal-15.09.1076.9220f03-i686-linux.iso",
+  iso_sha256: "4ffa1cbaa006bd6d804ecbf3da3c613fbf57602e9c383e5d985f2fbd6ff265f5",
   virtualbox_guest_os: "Linux",
 )
--- nixos-x86_64.rb
+++ nixos-x86_64.rb
@@ -3,7 +3,7 @@ require './gen_template.rb'
 
 gen_template(
   arch: "x86_64",
-  iso_url: "https://nixos.org/releases/nixos/15.09/nixos-15.09.597.71e29b4/nixos-minimal-15.09.597.71e29b4-x86_64-linux.iso",
-  iso_sha256: "c35133fb1e479ea9f42ffef9e4b06b6304914bbecb5337dfb75fe65fdf5298f5",
+  iso_url: "https://nixos.org/releases/nixos/15.09/nixos-15.09.1076.9220f03/nixos-minimal-15.09.1076.9220f03-x86_64-linux.iso",
+  iso_sha256: "77bd77859f937da23e9a45ccf50410ae32a41400e3b9ca3a8de212821b641c88",
   virtualbox_guest_os: "Linux_64",
 )
