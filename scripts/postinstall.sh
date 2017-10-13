3f74e3ffd664acc2a8b8f7bfcafb7d8fa0b99571
Pierre Radermecker <pierrer@pi3r.be>
2017-10-13 09:11:34 +0200

    Update to nixos 17.09

--- scripts/postinstall.sh
+++ scripts/postinstall.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Make sure we are totally up to date
-nix-channel --add http://nixos.org/channels/nixos-17.03 nixos
+nix-channel --add http://nixos.org/channels/nixos-17.09 nixos
 nix-channel --update
 nixos-rebuild switch --upgrade
 
