600d6603fd11694deb2fe74b6c806d7f1a272ffe
Pierre Radermecker <pierrer@pi3r.be>
2018-12-19 18:12:22 +0100

    Add custom configuration hook file
    This is handy to configure locale, hypervisor or any other organisation specific
    configuration.

--- scripts/install.sh
+++ scripts/install.sh
@@ -22,11 +22,13 @@ mount LABEL=nixos /mnt
 
 # Setup system
 nixos-generate-config --root /mnt
+
 curl -sf "$packer_http/vagrant.nix" > /mnt/etc/nixos/vagrant.nix
 curl -sf "$packer_http/vagrant-hostname.nix" > /mnt/etc/nixos/vagrant-hostname.nix
 curl -sf "$packer_http/vagrant-network.nix" > /mnt/etc/nixos/vagrant-network.nix
 curl -sf "$packer_http/builders/$PACKER_BUILDER_TYPE.nix" > /mnt/etc/nixos/hardware-builder.nix
 curl -sf "$packer_http/configuration.nix" > /mnt/etc/nixos/configuration.nix
+curl -sf "$packer_http/custom-configuration.nix" > /mnt/etc/nixos/custom-configuration.nix
 
 ### Install ###
 nixos-install
