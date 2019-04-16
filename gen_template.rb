c7eed0479c19fadd8c07503aca9eb9d08f4adde4
zimbatm <zimbatm@zimbatm.com>
2019-04-17 00:41:04 +0200

    Revert "remove the post-processors"
    This reverts commit dfc1beca4bef917756f2c9f2a7a7a4abdfbcfa47.

--- gen_template.rb
+++ gen_template.rb
@@ -85,6 +85,12 @@ def gen_template(
     provisioners: [
       { type: 'shell', script: './scripts/install.sh' }
     ],
+    'post-processors': [[
+      {
+        type: 'vagrant',
+        keep_input_artifact: false,
+      }
+    ]],
   )
 end
 
