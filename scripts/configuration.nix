54da7db36988c18a52a81ddd8a1dbff68a97c150
Pi3r <pierrer@pi3r.be>
2020-05-06 15:23:51 +0200

    Fix warning message (#60)
    See https://nixos.org/nixos/manual/release-notes.html#sec-release-20.03-incompatibilities

--- scripts/configuration.nix
+++ scripts/configuration.nix
@@ -41,25 +41,23 @@
     rsync
   ];
 
-  # Creates a "vagrant" user with password-less sudo access
-  users = {
-    extraGroups = [ { name = "vagrant"; } ];
-    extraUsers  = [
-      # Try to avoid ask password
-      { name = "root"; password = "vagrant"; }
-      {
-        description     = "Vagrant User";
-        name            = "vagrant";
-        group           = "vagrant";
-        extraGroups     = [ "users" "wheel" ];
-        password        = "vagrant";
-        home            = "/home/vagrant";
-        createHome      = true;
-        useDefaultShell = true;
-        openssh.authorizedKeys.keys = [
+  users.users.root = { password = "vagrant"; };
+  # Creates a "vagrant" group & user with password-less sudo access
+  users.groups.vagrant = {
+    name = "vagrant";
+    members = [ "vagrant" ];
+  };
+  users.users.vagrant = {
+    description     = "Vagrant User";
+    name            = "vagrant";
+    group           = "vagrant";
+    extraGroups     = [ "users" "wheel" ];
+    password        = "vagrant";
+    home            = "/home/vagrant";
+    createHome      = true;
+    useDefaultShell = true;
+    openssh.authorizedKeys.keys = [
           "ssh-rsa AAAAB3NzaC1yc2EAAAABIwAAAQEA6NF8iallvQVp22WDkTkyrtvp9eWW6A8YVr+kz4TjGYe7gHzIw+niNltGEFHzD8+v1I2YJ6oXevct1YeS0o9HZyN1Q9qgCgzUFtdOKLv6IedplqoPkcmF0aYet2PkEDo3MlTBckFXPITAMzF8dJSIFo9D8HfdOV0IAdx4O7PtixWKn5y2hMNG0zQPyUecp4pzC6kivAIhyfHilFR61RGL+GPXQ2MWZWFYbAGjyiYJnAmCP3NOTd0jMZEnDkbUvxhMmBYSdETk1rRgm+R4LOzFUGaHqHDLKLX+FIPKcF96hrucXzcWyLbIbEgE98OHlnVYCzRdK8jlqm8tehUc9c9WhQ== vagrant insecure public key"
-        ];
-      }
     ];
   };
 
