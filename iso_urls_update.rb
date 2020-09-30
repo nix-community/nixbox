a7feea8c9478c034d55154312e40454fb01e306c
Sandro Jäckel <sandro.jaeckel@gmail.com>
2020-09-30 22:20:43 +0200

    Fix selector for new website

--- iso_urls_update.rb
+++ iso_urls_update.rb
@@ -12,7 +12,7 @@ include REXML
 isos = {}
 
 xml = Document.new(open("https://nixos.org/nixos/download.html"))
-xml.root.elements.each("body/*/*/*/ul/li") { |li|
+xml.root.elements.each("body/*/*/*/*/*/ul/li") { |li|
   hrefs = li.get_elements("a").map { |a| a.attribute("href") }
   if hrefs.size == 2 and /.*minimal.*/i =~ hrefs[0].value
   then
