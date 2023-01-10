4dba0e99d1e3a2b6253101306e4f2bbdd07f520d
francocalvo <100273133+francocalvo@users.noreply.github.com>
2023-01-10 16:40:35 -0300

    fix bash variable in install.sh (#84)

--- scripts/install.sh
+++ scripts/install.sh
@@ -22,7 +22,7 @@ mount LABEL=nixos /mnt
 # Setup system
 nixos-generate-config --root /mnt
 
-curl -sf "$PACKER_HTTP_ADDR_ADDR/vagrant.nix" > /mnt/etc/nixos/vagrant.nix
+curl -sf "$PACKER_HTTP_ADDR/vagrant.nix" > /mnt/etc/nixos/vagrant.nix
 curl -sf "$PACKER_HTTP_ADDR/vagrant-hostname.nix" > /mnt/etc/nixos/vagrant-hostname.nix
 curl -sf "$PACKER_HTTP_ADDR/vagrant-network.nix" > /mnt/etc/nixos/vagrant-network.nix
 curl -sf "$PACKER_HTTP_ADDR/builders/$PACKER_BUILDER_TYPE.nix" > /mnt/etc/nixos/hardware-builder.nix
